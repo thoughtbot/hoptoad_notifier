d34b742dab0b9002c7201c86cd076e831c4d9ec4
Jon Yurek <jyurek@thoughtbot.com>
2011-06-06 13:05:58 -0400

    Actually use the streamer in the UserInformer

--- lib/hoptoad_notifier/user_informer.rb
+++ lib/hoptoad_notifier/user_informer.rb
@@ -11,12 +11,7 @@ module HoptoadNotifier
     def call(env)
       status, headers, body = @app.call(env)
       if env['hoptoad.error_id'] && HoptoadNotifier.configuration.user_information
-        new_body = []
-        body.each do |chunk|
-          new_body << chunk.gsub("<!-- HOPTOAD ERROR -->", replacement(env['hoptoad.error_id']))
-        end
-        headers['Content-Length'] = new_body.sum(&:length).to_s
-        body = new_body
+        body = UserInformerStreamer.new(body, replacement(env['hoptoad.error_id']))
       end
       [status, headers, body]
     end
